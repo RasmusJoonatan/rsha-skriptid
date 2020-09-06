3215210a5c36f3ed5b76369ae2c96de671ada7e1
RasmusJoonatan <48347134+RasmusJoonatan@users.noreply.github.com>
2020-09-06 19:33:23 +0300

    Create apache_paigaldus.sh

---
+++ apache_paigaldus.sh
@@ -0,0 +1,19 @@
+#kontrollib kas apache on installitud
+APACHE2=$(dpkg-query -W -f="${Status}" apache2 2>/dev/null | grep -c "ok installed")
+#kui väärus on 0 siis apache pole installitud
+if [ $APACHE2 -eq 0 ]; then
+#ütleb et hakkab installima
+echo "Paigaldame apache2"
+apt install apache2
+echo "Apache on paigaldatud"
+#peale apache installimist käivitab selle ja näitab statust
+service apache2 start
+service apache2 status
+#kui vväärtus on 1 siis apache on installitud ja annab teate
+elif [ $APACHE2 -eq 1 ]; then
+echo "Apache on seadme juba olemas"
+#teeb apachele restardi ja näitab statust
+service apache2 start
+service apache2 status
+#lõpp
+fi
