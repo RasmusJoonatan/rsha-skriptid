c511d3657073053e4cd4dd1915bf44412bdd290c
RasmusJoonatan <48347134+RasmusJoonatan@users.noreply.github.com>
2020-09-08 19:46:37 +0300

    Skript mis paigaldab mysql-serveri

---
+++ mysql_paigaldamine.sh
@@ -0,0 +1,22 @@
+# mysql-serveri paigaldamine
+MYSQL=$(dpkg-query -W -f='${status}' mysql-server 2>/dev/null | grep -c 'ok installed')
+# Kui väärtus = 0
+if [ $MYSQL -eq 0 ]; then
+	# installitakse mysql server
+	echo "Paigaldatakse mysql ja lisad!"
+	apt install mysql-server
+	echo "mysql on paigaltatud"
+	# ilma kasutaja ja paroolita
+	touch $Home/.my.cnf
+	echo "[client]" >> $Home/.my.cnf
+	echo "host = localhost" >> $Home/.my.cnf
+	echo "user = root" >> $Home/.my.cnf
+	echo "password = qwerty" >> $Home/.my.cnf
+# Kui väärtus = 1
+elif [ $MYSQL -eq 1 ]; then
+	# Siis kontrollitakse mysql olemasolu
+	echo "mysql server on juba installitud"
+	mysql
+fi
+
+# skripti lõpp
