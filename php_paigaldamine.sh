1d932ca7be8400eb965e8559d19671bcf9675849
RasmusJoonatan <48347134+RasmusJoonatan@users.noreply.github.com>
2020-09-08 19:49:55 +0300

    Skript mis paigaldab php

---
+++ php_paigaldamine.sh
@@ -0,0 +1,19 @@
+# php paigaldusskript
+PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
+# kui PHP muutuja väärtus võrdub 0-ga
+if [ $PHP -eq 0 ]; then
+# siis ok installed kui ei ole leitud
+# ja väljastame vastava teate ning
+# paigaldame teenuse
+echo "Paigaldame php ja vajalikud lisad"
+apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
+echo "php on paigaldatud"
+# kui PHP muutuja väärtus võrdub 1-ga
+elif [ $PHP -eq 1 ]; then
+# kontrollime olemasolu
+echo "php on juba paigaldatud"
+      
+which php
+        
+fi
+#skripti lõpp
