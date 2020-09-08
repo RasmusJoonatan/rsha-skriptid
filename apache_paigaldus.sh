a32ab247dd19b760ae15d54889bbdf124d377f3d
RasmusJoonatan <48347134+RasmusJoonatan@users.noreply.github.com>
2020-09-08 19:50:21 +0300

    Update apache_paigaldus.sh

--- apache_paigaldus.sh
+++ apache_paigaldus.sh
@@ -3,15 +3,15 @@
 APACHE2=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
 # Kui muutuja väärtus = 0-ga
 if [ $APACHE2 -eq 0 ]; then
-	echo "Apache2 paigaldamine"
-	apt install apache2
-	echo "Apache on paigaldatud"
+echo "Apache2 paigaldamine"
+apt install apache2
+echo "Apache on paigaldatud"
 # Kui muutuja väärtus = 1-ga
 # samuti kontrollitakse, kas teenus on aktiivne ilma veateadeteta
 elif [ $APACHE2 -eq 1 ]; then
-	echo "Apache on juba installitud"
-	service apache2 start
-	service apache2 status
+echo "Apache on juba installitud"
+service apache2 start
+service apache2 status
 
 fi
 
